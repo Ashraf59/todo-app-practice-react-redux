98d7dc8e4f8219b77c0c90bd4aa3c10587c96b87
Ashraf59 <ashrafams59b@gmail.com>
2023-03-06 12:41:16 +0600

    Complete todo app practice

---
+++ src/redux/rootReducer.js
@@ -0,0 +1,13 @@
+
+import todosReducer from '../redux/todos/reducer';
+import filtersReducer from '../redux/filters/reducer';
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+
+    todos: todosReducer,
+    filters: filtersReducer
+
+})
+
+export default rootReducer;
---
+++ src/redux/todos/initialState.js
@@ -0,0 +1,16 @@
+
+export const initialState = [
+    {
+        id: 1,
+        text: "React JS",
+        completed: true,
+
+    },
+    {
+        id: 2,
+        text: "Redux",
+        completed: false,
+        color: "red"
+    }
+]
+
